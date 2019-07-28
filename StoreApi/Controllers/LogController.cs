4912ce556fdeb6c394396c71efe30f9e659b7d79
Sergio Urrutia <sergio.urrutiaw@gmail.com>
2019-07-28 12:30:32 -0600

    Adding controller to get logs

---
+++ StoreApi/Controllers/LogController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Store.Core.Interfaces;
+using Store.Core.Product;
+
+namespace StoreApi.Controllers
+{
+    [Route("logs")]
+    public class LogController : BaseController
+    {
+        private readonly IRepository<PriceUpdateLogEntity> _priceUpdateLogRepository;
+        private readonly IRepository<PurchaseLogEntity> _purchaseLogRepository;
+
+        public LogController(IRepository<PurchaseLogEntity> purchaseLogRepository, IRepository<PriceUpdateLogEntity> priceUpdateLogRepository)
+        {
+            _priceUpdateLogRepository = priceUpdateLogRepository;
+            _purchaseLogRepository = purchaseLogRepository;
+        }
+
+        [HttpGet]
+        [Route("price")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetPriceUpdateLogs()
+        {
+            return Ok(_priceUpdateLogRepository.FindAll());
+        }
+
+        [HttpGet]
+        [Route("purchase")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetPurchaseLogs()
+        {
+            return Ok(_purchaseLogRepository.FindAll());
+        }
+    }
+}
